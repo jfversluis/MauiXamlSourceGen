61cafe924285854032cc340be5caefe3327cfda5
Gerald Versluis <gerald.versluis@microsoft.com>
2025-03-26 16:50:12 +0100

    Initial commit

---
+++ MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+[assembly: XamlProcessing(XamlInflator.SourceGen)]
+namespace MauiXamlSourceGen;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
